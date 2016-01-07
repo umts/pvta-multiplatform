0c2bf5e222a20220dd390bac6c5a3520224263b6
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-07 18:15:41 -0500

    Added some details to the mapView

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -99,8 +99,24 @@ angular.module('starter.controllers', ['starter.services'])
     };
  
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
-  }, function(error){
-    console.log("Could not get location");
-  });
+    
+    //Wait until the map is loaded
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+      var marker = new google.maps.Marker({
+      map: $scope.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng
+      });
+      var infoWindow = new google.maps.InfoWindow({
+        content: "Here I am!"
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open($scope.map, marker);
+      });
+    });
+
+    }, function(error){
+      console.log("Could not get location");
+    });
 });
