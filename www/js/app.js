05ec7abd7b8259b03b721ea47a0147cc971d3541
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-12 16:33:47 -0500

    Used menu.html's menu bar directive to turn all menu bars blue and fix an error that caused the sidemenu to disappear in Search

--- www/js/app.js
+++ www/js/app.js
@@ -110,5 +110,5 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'angular
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/stopDepartures/96');
+  $urlRouterProvider.otherwise('/app/search');
 });
