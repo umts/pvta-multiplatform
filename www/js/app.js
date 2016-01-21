3e31bbb021b820e252baec39fb672c2a11c13ba4
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-21 13:41:49 -0500

    Added a controllers directory, added the SearchController to a new file within it. Added the file to index.html as an import and set app.js to use the new file instead of the old one contained in controllers.js

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+angular.module('starter.controllers', ['starter.services']);
+
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'angularMoment', 'jett.ionic.filter.bar'])
 
 .run(function($ionicPlatform) {
@@ -37,7 +39,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'angular
     views: {
       'menuContent': {
         templateUrl: 'templates/search.html',
-        controller: 'SearchCtrl'
+        controller: 'SearchController'
       }
     }
   })
