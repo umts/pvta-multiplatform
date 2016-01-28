e4d8e390ce596506149dfbd299d427f01ceafcd0
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-01-28 16:56:59 -0500

    Routes page is now searchable

--- www/js/controllers/routes-controller.js
+++ www/js/controllers/routes-controller.js
@@ -1,10 +1,41 @@
-angular.module('pvta.controllers').controller('RoutesController', function($scope, $resource, Routes, RouteList){
+angular.module('pvta.controllers').controller('RoutesController', function($scope, $resource, Routes, RouteList, $ionicFilterBar){
+  var filterBarInstance;
+  
   if(RouteList.isEmpty()){
-    $scope.routes = Routes.query(function(){
-      RouteList.pushEntireList($scope.routes);
+    $scope.all = Routes.query(function(){
+      RouteList.pushEntireList($scope.all);
+      for(var route = 0; route < $scope.all.length; route++){
+        $scope.all[route] = {
+          LongName: $scope.all[route].LongName,
+          ShortName: $scope.all[route].ShortName,
+          RouteId: $scope.all[route].RouteId,
+          Color: $scope.all[route].Color,
+        }
+      }
     });
   }
   else{
-    $scope.routes = RouteList.getEntireList();
+    $scope.all = RouteList.getEntireList();
   }
+  
+  $scope.showFilterBar = function () {
+      filterBarInstance = $ionicFilterBar.show({
+        items: $scope.all,
+        update: function (filteredItems, filterText) {
+          $scope.all = filteredItems;
+        }
+      });
+    };
+  $scope.refreshItems = function () {
+      if (filterBarInstance) {
+        filterBarInstance();
+        filterBarInstance = null;
+      }
+
+      $timeout(function () {
+        getItems();
+        $scope.$broadcast('scroll.refreshComplete');
+      }, 1000);
+    };
+  
 })
