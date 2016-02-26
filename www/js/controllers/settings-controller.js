ff66cc2727c45e44ab1693baaf8bb1bab075c98a
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-02-26 18:22:15 -0500

    Added a setting that allows choice of autorefresh times. Saves this time to localforage. Doesn't reload with the proper time yet, but it IS saved.

--- www/js/controllers/settings-controller.js
+++ www/js/controllers/settings-controller.js
@@ -1,3 +1,13 @@
 angular.module('pvta.controllers').controller('SettingsController', function($scope){
   
+  
+  $scope.autorefresh = 45000;
+  
+  $scope.updateRefresh = function(val){
+    console.log("updaterefresh called");
+    localforage.setItem('autoRefresh', val, function(err, value){
+      console.log(err + value);
+    })
+  }
+  
 });
