7481e1eb905583039e45ba82a7fe55f168bcbf88
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-12 14:59:05 -0500

    Moved the new calls into the Stops controller and removed old code from it

--- www/js/controllers/stops-controller.js
+++ www/js/controllers/stops-controller.js
@@ -1,20 +1,13 @@
-angular.module('pvta.controllers').controller('StopsController', function ($scope, $resource, StopList, Stops, NearestStops, $ionicFilterBar, $cordovaGeolocation) {
+angular.module('pvta.controllers').controller('StopsController', function ($scope, $resource, Stops, NearestStops, $ionicFilterBar, $cordovaGeolocation, StopsForage) {
   $scope.display_message = 'No results found.';
   var filterBarInstance;
-  if (StopList.isEmpty()) {
-    $cordovaGeolocation.getCurrentPosition().then(function (position) {
-      NearestStops.query({latitude: position.coords.latitude, longitude: position.coords.longitude}, function (stops) {
-        $scope.stops = StopList.pushEntireList(stops);
-      });
-    }, function (err) {
-      Stops.query(function (stops) {
-        $scope.stops = StopList.pushEntireList(stops);
-      });
+
+  $cordovaGeolocation.getCurrentPosition().then(function (position) {
+    StopsForage.get(position.coords.latitude, position.coords.longitude).then(function(stops){
+      $scope.stops = stops;
+      StopsForage.save(stops);
     });
-  }
-  else {
-    $scope.stops = StopList.getEntireList();
-  }
+  });
 
   $scope.showFilterBar = function () {
     filterBarInstance = $ionicFilterBar.show({
