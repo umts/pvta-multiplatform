5c7232dc975bd659e00ea2113d512d4f4526b50e
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-23 23:19:30 -0400

    Added a . to Stops that resolves a list never appearing until the dom gets manipulated into reloading itself

--- www/js/controllers/stops-controller.js
+++ www/js/controllers/stops-controller.js
@@ -1,9 +1,9 @@
 angular.module('pvta.controllers').controller('StopsController', function ($scope, $resource, Stops, NearestStops, $ionicFilterBar, $cordovaGeolocation, StopsForage) {
-  $scope.display_message = 'No results found.';
   var filterBarInstance;
   $cordovaGeolocation.getCurrentPosition().then(function (position) {
     StopsForage.get(position.coords.latitude, position.coords.longitude).then(function(stops){
       $scope.stops = stops;
+      $scope.$apply();
       StopsForage.save(stops);
     });
   });
