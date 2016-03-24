29198b1c2dbdf7719ab6689858297891fefa0d42
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-24 14:07:28 -0400

    Added a timeout to the stops location finder to avoid just never getting stops if location is never pinpointed

--- www/js/controllers/stops-controller.js
+++ www/js/controllers/stops-controller.js
@@ -1,11 +1,13 @@
 angular.module('pvta.controllers').controller('StopsController', function ($scope, $resource, Stops, NearestStops, $ionicFilterBar, $cordovaGeolocation, StopsForage) {
   var filterBarInstance;
-  $cordovaGeolocation.getCurrentPosition().then(function (position) {
+  $cordovaGeolocation.getCurrentPosition({timeout: 3000}).then(function (position) {
     StopsForage.get(position.coords.latitude, position.coords.longitude).then(function(stops){
       $scope.stops = stops;
       $scope.$apply();
       StopsForage.save(stops);
     });
+  }, function(err){
+    console.log(JSON.stringify(err));
   });
   $scope.showFilterBar = function () {
     filterBarInstance = $ionicFilterBar.show({
