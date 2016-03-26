03cfc912fb2997978a18f604a1d7b8fb949b1b4e
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-03-25 23:28:28 -0400

    Removed useless vars in Stops

--- www/js/controllers/stops-controller.js
+++ www/js/controllers/stops-controller.js
@@ -1,19 +1,18 @@
 angular.module('pvta.controllers').controller('StopsController', function ($scope, $resource, Stops, NearestStops, $ionicFilterBar, $cordovaGeolocation, StopsForage) {
-  var filterBarInstance;
   $cordovaGeolocation.getCurrentPosition({timeout: 3000}).then(function (position) {
     StopsForage.get(position.coords.latitude, position.coords.longitude).then(function (stops) {
       $scope.stops = stops;
       $scope.$apply();
       StopsForage.save(stops);
     });
-  }, function (err) {
+  }, function () {
     StopsForage.get().then(function (stops) {
       $scope.stops = stops;
       StopsForage.save(stops);
     });
   });
   $scope.showFilterBar = function () {
-    filterBarInstance = $ionicFilterBar.show({
+    $ionicFilterBar.show({
       items: $scope.stops,
       update: function (filteredItems, filterText) {
         $scope.stops = filteredItems;
