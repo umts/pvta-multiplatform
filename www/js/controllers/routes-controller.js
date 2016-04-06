09c99f6f7c0a6715b75e77d2a2e7728351da4fa6
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-04-06 12:00:08 -0400

    Added  spinner to routes to coax the dom into refreshing and displaying routes when it finishes preparing them

--- www/js/controllers/routes-controller.js
+++ www/js/controllers/routes-controller.js
@@ -1,17 +1,16 @@
-angular.module('pvta.controllers').controller('RoutesController', function ($scope, $resource, Routes, RouteList, $ionicFilterBar, RouteForage) {
+angular.module('pvta.controllers').controller('RoutesController', function ($scope, $resource, Routes, RouteList, $ionicFilterBar, RouteForage, $ionicLoading) {
   var filterBarInstance;
-
+  $ionicLoading.show({});
   RouteForage.get().then(function (routes) {
     RouteForage.save(routes);
     $scope.routes = stripDetails(routes);
+    $ionicLoading.hide();
   });
-
   function stripDetails (routeList) {
     return _.map(routeList, function (route) {
       return _.pick(route, 'RouteId', 'ShortName', 'LongName', 'Color');
     });
   }
-
   $scope.showFilterBar = function () {
     filterBarInstance = $ionicFilterBar.show({
       items: $scope.routes,
