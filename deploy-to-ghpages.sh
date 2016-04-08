a8cc7e2bcce37264f4d11e403feef7dbf94ced69
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-04-08 19:22:05 -0400

    Updated deploy again

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd ../
 mkdir multiplatform-deploy-tmp
+echo Making a copy of pvta-multiplatform to be safe
 cp -r pvta-multiplatform multiplatform-deploy-tmp
+echo Done copying!
 cd multiplatform-deploy-tmp/pvta-multiplatform
 echo Type the branch you want to deploy to gh-pages and press RETURN.
 read branch
@@ -12,13 +14,24 @@ mv www/* .
 rm -rf www
 git add -A
 commit="Deploying $branch to gh-pages"
-echo If you would like to include a commit message, add one and press RETURN
+echo If you would like to include a commit message, add one and press RETURN.
 read personalized
 if [ -z "$personalized" ]; then
   $commit = "$commit: $personalized"
 fi
 git commit -m "$commit"
+echo Done committing.
+echo Ready to push? Caution: your changes will be live on the internet! [y/n]
+read confirm
+if ["$confirm"="y"]; then
+  git push origin gh-pages
+  echo Done pushing.
+else
+  echo Oh well. Run this script again later when ready.
+fi
 cd ../../
+echo --------------------
+echo Deleting the temporary repo copy.
 rm -rf multiplatform-deploy-tmp
+echo Done deleting.
 cd pvta-multiplatform
-echo Done
