4d7cd4e5f8c5c6712f51354a80d0ce953635554b
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-04-08 19:25:54 -0400

    Updated deploy again

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -9,7 +9,8 @@ echo Type the branch you want to deploy to gh-pages and press RETURN.
 read branch
 git checkout $branch
 git branch -D gh-pages
-git checkout -b gh-pages
+git checkout -b gh-pages]
+git pull origin gh-pages
 mv www/* .
 rm -rf www
 git add -A
