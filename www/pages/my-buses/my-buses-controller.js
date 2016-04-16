429b8f73beffdfb35f1f9fc4c5b6baf924f27cd8
Brian Gregg <bgregg@umass.edu>
2016-04-16 19:32:25 -0400

    added comments

--- www/pages/my-buses/my-buses-controller.js
+++ www/pages/my-buses/my-buses-controller.js
@@ -32,9 +32,12 @@ angular.module('pvta.controllers').controller('MyBusesController', function ($sc
          * routes array, this alert will
          * appear on the page twice.
          */
+
+         //Also if there are no routes for that alert , show it by default
          if(alert.Routes.length == 0){
            $scope.messages.push(alert);
          }
+
          else{
            _.each(alert.Routes, function (routeId) {
              if (_.contains(routes, routeId)) {
