637e862cdaaabb1d44ca02c48308c9ca98b63da2
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-04-28 11:36:09 -0400

    Deploy script debugging

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $my_dir
+echo $my_dir
 if [[ ! "$(git status -sb | head -1 )" == "## master"* ]]
 then
   echo "Not currently on master branch"
@@ -19,6 +20,7 @@ bower install
 shopt -s dotglob && git rm -rf --ignore-unmatch *
 git checkout master www/
 mv www/* ./
+read k
 rm -rf www
 git checkout master scss
 git add -A
