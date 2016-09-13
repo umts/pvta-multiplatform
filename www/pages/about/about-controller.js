dd14d6b613dc1cc8cd5b2adb60ae6f1da3d25a32
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-09-13 00:27:24 -0400

    Fix CC issues

--- www/pages/about/about-controller.js
+++ www/pages/about/about-controller.js
@@ -7,22 +7,20 @@ angular.module('pvta.controllers').controller('AboutController', function ($scop
     Opens a given URL in the browser.
   */
   $scope.openLink = function(url) {
+    console.log('Opening ' + url + ' in the browser');
     // If we're on a device, we have to use cordova
     // to open the default browser
     if ($window.cordova) {
       // We have to make sure that cordova is properly loaded before
       // using one of its plugins.
-      document.addEventListener("deviceready", onDeviceReady, false);
-      function onDeviceReady() {
-        // Open the given URL in the system browser
-        console.log('Opening ' + url + 'in the browser');
+      document.addEventListener('deviceready', function () {
         cordova.InAppBrowser.open(url, '_system');
-      }
+      }, false);
     }
     // If we're running as a web-app in a standard browser,
     // we can just open the link in a new tab.
     else {
       $window.open(url);
     }
-  }
+  };
 });
