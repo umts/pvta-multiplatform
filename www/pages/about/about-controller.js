e88fbba9306217585c9ee24edf91908555bcd4fd
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-09-12 23:19:19 -0400

    Make the distinction between browser and mobile app when opening a url in the browser

--- www/pages/about/about-controller.js
+++ www/pages/about/about-controller.js
@@ -1,4 +1,4 @@
-angular.module('pvta.controllers').controller('AboutController', function ($scope, Info) {
+angular.module('pvta.controllers').controller('AboutController', function ($scope, $window, Info) {
   ga('set', 'page', '/about.html');
   ga('send', 'pageview');
   $scope.vNum = Info.versionNum;
@@ -8,13 +8,18 @@ angular.module('pvta.controllers').controller('AboutController', function ($scop
     URL in the device's default browser.
   */
   $scope.openLink = function(url) {
-    // We have to make sure that cordova is properly loaded before
-    // using one of its plugins.
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-      // Open the given URL in the system browser
-      console.log('Opening ' + url + 'in the browser');
-      cordova.InAppBrowser.open(url, '_system');
+    if (window.cordova) {
+      // We have to make sure that cordova is properly loaded before
+      // using one of its plugins.
+      document.addEventListener("deviceready", onDeviceReady, false);
+      function onDeviceReady() {
+        // Open the given URL in the system browser
+        console.log('Opening ' + url + 'in the browser');
+        cordova.InAppBrowser.open(url, '_system');
+      }
+    }
+    else {
+      $window.open(url);
     }
   }
 });
