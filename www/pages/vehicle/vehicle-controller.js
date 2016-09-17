e5c231d2596f793ebea28bcd64972fdc45f9f353
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-09-17 15:18:17 -0400

    Started redoing ui for vehicle page and added some properties to the page

--- www/pages/vehicle/vehicle-controller.js
+++ www/pages/vehicle/vehicle-controller.js
@@ -19,4 +19,18 @@ angular.module('pvta.controllers').controller('VehicleController', function ($sc
     $scope.$broadcast('scroll.refreshComplete');
   };
 
+  $scope.isArticulated = function (nameString) {
+    var nameInt = parseInt(nameString);
+    // Match UMass Transit articulated buses
+    if (nameInt >= 3400 && nameInt < 3500) {
+      return true;
+    }
+    // Match VatCo articulated buses
+    else if (nameInt >= 7900 && nameInt < 8000) {
+      return true;
+    }
+    // There are no SatCo articulated buses a/o 2016.
+    else return false;
+  }
+
 });
