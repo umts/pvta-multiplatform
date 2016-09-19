17af31440d148b105178ebd367121b12654db441
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-09-19 19:18:34 -0400

    Call getVehicles less often, use the pulltorefresh loader for it instead of the awkward flash that appears currently

--- www/pages/route/route-controller.js
+++ www/pages/route/route-controller.js
@@ -1,11 +1,18 @@
 angular.module('pvta.controllers').controller('RouteController', function($scope, $state, $stateParams, $ionicLoading, Route, RouteVehicles, FavoriteRoutes, Messages){
   ga('set', 'page', '/route.html');
   ga('send', 'pageview');
-  $ionicLoading.show();
-  var getVehicles = function(){
-    $scope.vehicles = RouteVehicles.query({id: $stateParams.routeId});
+
+  /*
+  * Called when the user performs a pull-to-refresh.  Only downloads
+  * vehicle data instead of all route data.
+  */
+  function getVehicles (){
+    $scope.vehicles = RouteVehicles.query({id: $stateParams.routeId}, function () {
+    $scope.$broadcast('scroll.refreshComplete');
+    });
   };
 
+  $ionicLoading.show();
   var route = Route.get({routeId: $stateParams.routeId}, function() {
     route.$save();
     getHeart();
@@ -58,11 +65,9 @@ angular.module('pvta.controllers').controller('RouteController', function($scope
 
   $scope.refresh = function(){
     getVehicles();
-    $scope.$broadcast('scroll.refreshComplete');
   };
 
   $scope.$on('$ionicView.enter', function(){
     getHeart();
-    getVehicles();
   });
 });
