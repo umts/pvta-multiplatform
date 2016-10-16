a12ad354d5bf249b9bc0de9807712dba023be659
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-10-16 15:01:04 -0400

    Managed to get stops in order, with some icons denoting timechecks. Not centered, though?

--- www/pages/route/route-controller.js
+++ www/pages/route/route-controller.js
@@ -67,4 +67,67 @@ angular.module('pvta.controllers').controller('RouteController', function($scope
   $scope.$on('$ionicView.enter', function(){
     getHeart();
   });
+  $scope.timeline = [{
+    date: new Date(),
+    title: "I am here",
+    author: "Ludo Anderson",
+    profilePicture: "https://upload.wikimedia.org/wikipedia/en/7/70/Shawn_Tok_Profile.jpg",
+    text: "Lorem ipsum dolor sit amet",
+    type: "location"
+
+  }, {
+    date: new Date(),
+    title: "For my friends",
+    author: "Sara Orwell",
+    profilePicture: "https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    type: "text"
+
+  }, {
+    date: new Date(),
+    title: "Look at my video!",
+    author: "Miranda Smith",
+    profilePicture: "https://static.licdn.com/scds/common/u/images/apps/plato/home/photo_profile_headshot_200x200_v2.jpg",
+    text: "Lorem ipsum dolor sit amet",
+    type: "video"
+
+  }, {
+    date: new Date(),
+    title: "Awesome picture",
+    author: "John Mybeweeg",
+    profilePicture: "http://www.lawyersweekly.com.au/images/LW_Media_Library/LW-602-p24-partner-profile.jpg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    type: "picture"
+  }]
+})
+
+.controller('IconsCtrl', function($scope) {
+  $scope.timeline = [{
+    date: new Date(),
+    title: "Awesome picture",
+    author: "John Mybeweeg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    type: "picture"
+  }, {
+    date: new Date(),
+    title: "Look at my video!",
+    author: "Miranda Smith",
+    text: "Lorem ipsum dolor sit amet",
+    type: "video"
+
+  }, {
+    date: new Date(),
+    title: "I am here",
+    author: "Ludo Anderson",
+    text: "Lorem ipsum dolor sit amet",
+    type: "location"
+
+  }, {
+    date: new Date(),
+    title: "For my friends",
+    author: "Sara Orwell",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    type: "text"
+
+  }]
 });
