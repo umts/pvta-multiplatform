5d124d7dfb819196d5254bba7bf95c826772aa48
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-10-16 15:44:28 -0400

    Readd css for timeline, minifying it screwed it up.  Also hrefs on stops and some ui edge case work

--- www/pages/route/route-controller.js
+++ www/pages/route/route-controller.js
@@ -13,11 +13,11 @@ angular.module('pvta.controllers').controller('RouteController', function($scope
   };
 
   $ionicLoading.show();
-  var route = Route.get({routeId: $stateParams.routeId}, function() {
-    route.$save();
+  Route.get({routeId: $stateParams.routeId}, function(route) {
+    $scope.route = route
     getHeart();
-    $scope.stops = route.Stops;
-    $scope.vehicles = route.Vehicles;
+    $scope.stops = $scope.route.Stops;
+    $scope.vehicles = $scope.route.Vehicles;
 
     // Need route to be defined before we can filter messages
     var messages = Messages.query(function(){
@@ -30,7 +30,6 @@ angular.module('pvta.controllers').controller('RouteController', function($scope
       $scope.messages = filteredMessages;
     });
   });
-  $scope.route = route;
   $scope.stops = [];
 
   $scope.toggleGroup = function(group) {
@@ -55,7 +54,7 @@ angular.module('pvta.controllers').controller('RouteController', function($scope
   };
   $scope.liked = false;
   var getHeart = function(){
-    FavoriteRoutes.contains(route, function(bool){
+    FavoriteRoutes.contains($scope.route, function(bool){
       $scope.liked = bool;
     });
   };
@@ -67,67 +66,4 @@ angular.module('pvta.controllers').controller('RouteController', function($scope
   $scope.$on('$ionicView.enter', function(){
     getHeart();
   });
-  $scope.timeline = [{
-    date: new Date(),
-    title: "I am here",
-    author: "Ludo Anderson",
-    profilePicture: "https://upload.wikimedia.org/wikipedia/en/7/70/Shawn_Tok_Profile.jpg",
-    text: "Lorem ipsum dolor sit amet",
-    type: "location"
-
-  }, {
-    date: new Date(),
-    title: "For my friends",
-    author: "Sara Orwell",
-    profilePicture: "https://lh5.googleusercontent.com/-ZadaXoUTBfs/AAAAAAAAAAI/AAAAAAAAAGA/19US52OmBqc/photo.jpg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    type: "text"
-
-  }, {
-    date: new Date(),
-    title: "Look at my video!",
-    author: "Miranda Smith",
-    profilePicture: "https://static.licdn.com/scds/common/u/images/apps/plato/home/photo_profile_headshot_200x200_v2.jpg",
-    text: "Lorem ipsum dolor sit amet",
-    type: "video"
-
-  }, {
-    date: new Date(),
-    title: "Awesome picture",
-    author: "John Mybeweeg",
-    profilePicture: "http://www.lawyersweekly.com.au/images/LW_Media_Library/LW-602-p24-partner-profile.jpg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    type: "picture"
-  }]
-})
-
-.controller('IconsCtrl', function($scope) {
-  $scope.timeline = [{
-    date: new Date(),
-    title: "Awesome picture",
-    author: "John Mybeweeg",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    type: "picture"
-  }, {
-    date: new Date(),
-    title: "Look at my video!",
-    author: "Miranda Smith",
-    text: "Lorem ipsum dolor sit amet",
-    type: "video"
-
-  }, {
-    date: new Date(),
-    title: "I am here",
-    author: "Ludo Anderson",
-    text: "Lorem ipsum dolor sit amet",
-    type: "location"
-
-  }, {
-    date: new Date(),
-    title: "For my friends",
-    author: "Sara Orwell",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-    type: "text"
-
-  }]
 });
