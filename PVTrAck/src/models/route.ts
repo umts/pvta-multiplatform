bf1ff5e84addcb89137993dfc4325f568c93db51
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-12-13 22:59:53 -0500

    Scaffold - Add NONFUNCTIONAL route object model

---
+++ PVTrAck/src/models/route.ts
@@ -0,0 +1,12 @@
+export class Route {
+  shortName: string;
+  longName: string;
+  color: number;
+  routeId: number;
+  constructor(shortName: string, longName: string, color: number, routeId: number){
+    this.shortName = shortName;
+    this.longName = longName;
+    this.color = color;
+    this.routeId = routeId;
+  }
+}
