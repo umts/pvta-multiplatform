658886b20ba1aa677644f025eef6820bf826122b
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-12-13 21:54:54 -0500

    remove all ionic 1 stuff

--- www/directives/vehicle/vehicle-directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('pvta.directives').directive('vehicle', function () {
-  return {
-    scope: {
-      vehicle: '=data',
-      color: '=color'
-    },
-    templateUrl: 'directives/vehicle/vehicle-directive.html'
-  };
-});
