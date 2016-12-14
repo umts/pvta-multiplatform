8767abf7e9146b0e16afebd9db3fb39097ec00dc
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-12-13 22:33:29 -0500

    Scaffolding - Add all the root PVtrack pages to the sidemenu. Going between them all works! Fix plan trip name too.

--- PVTrAck/src/app/app.component.ts
+++ PVTrAck/src/app/app.component.ts
@@ -4,6 +4,18 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { About } from '../pages/about/about';
+import { Contact } from '../pages/contact/contact';
+import { MyBuses } from '../pages/my-buses/my-buses';
+import { PlanTrip } from '../pages/plan-trip/plan-trip';
+import { PrivacyPolicy } from '../pages/privacy-policy/privacy-policy';
+import { Route } from '../pages/route/route';
+import { RouteMap } from '../pages/route-map/route-map';
+import { RoutesAndStops } from '../pages/routes-and-stops/routes-and-stops';
+import { Settings } from '../pages/settings/settings';
+import { Stop } from '../pages/stop/stop';
+import { StopMap } from '../pages/stop-map/stop-map';
+import { StorageSettings } from '../pages/storage-settings/storage-settings';
 
 
 @Component({
@@ -22,7 +34,11 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Page Two', component: Page2 },
+      { title: 'My Buses', component: MyBuses },
+      { title: 'Routes and Stops', component: RoutesAndStops },
+      { title: 'Schedule', component: PlanTrip },
+      { title: 'Settings', component: Settings }
     ];
 
   }
