73a906b7f1684499e250a493e0e1973c82484dbf
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-12-13 22:25:47 -0500

    Scaffolding - Do all page TS imports in main app module TS

--- PVTrAck/src/app/app.module.ts
+++ PVTrAck/src/app/app.module.ts
@@ -3,6 +3,19 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { About } from '../pages/about/about';
+import { Contact } from '../pages/contact/contact';
+import { MyBuses } from '../pages/my-buses/my-buses';
+import { PlanTrip } from '../pages/plan-trip/plan-trip';
+import { PrivacyPolicy } from '../pages/privacy-policy/privacy-policy';
+import { Route } from '../pages/route/route';
+import { RouteMap } from '../pages/route-map/route-map';
+import { RoutesAndStops } from '../pages/routes-and-stops/routes-and-stops';
+import { Settings } from '../pages/settings/settings';
+import { Stop } from '../pages/stop/stop';
+import { StopMap } from '../pages/stop-map/stop-map';
+import { StorageSettings } from '../pages/storage-settings/storage-settings';
+
 
 @NgModule({
   declarations: [
