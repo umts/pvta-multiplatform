89cdef77f44161a935df3ea1fc99210cf07fb73c
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-12-15 00:17:19 -0500

    Starting to play around with downloading

--- src/pages/routes-and-stops/routes-and-stops.ts
+++ src/pages/routes-and-stops/routes-and-stops.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {Http, Response} from '@angular/http'
+
+
 @Component({
   selector: 'page-routes-and-stops',
   templateUrl: 'routes-and-stops.html'
 })
 export class RoutesAndStops {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.poo();
   }
+  private routesUrl = 'http://bustracker.pvta.com/infopoint/rest/routes/getvisibleroutes';  // URL to web API
 
+  poo() {
+    this.http.get('http://bustracker.pvta.com/infopoint/rest/routes/get/20030')
+      .subscribe(
+        data => {
+          let x: StopDeparture = data.json() as StopDeparture;
+          console.log(x)
+        },
+        err => console.log(err),
+        () => console.log('Random Quote Complete')
+      );
+    }
 }
