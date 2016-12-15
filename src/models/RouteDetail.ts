d10cb1d2488aa5e78d58358b75264c37c997e4b3
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-12-14 23:49:10 -0500

    Fill in vehicle, route, and routedetails

--- src/models/RouteDetail.ts
+++ src/models/RouteDetail.ts
@@ -20,10 +20,33 @@ class RouteDetails {
   Directions: any[];
   Vehicles: Vehicle[];
   Messages: Alert[];
-  constructor(shortName: string, longName: string, color: number, routeId: number){
-    this.shortName = shortName;
-    this.longName = longName;
-    this.color = color;
-    this.routeId = routeId;
+  constructor(RouteId: number, RouteRecordId: number, ShortName: string,
+    LongName: string, RouteAbbreviation: string, IvrDescription: string,
+    Color: string, TextColor: string, IsVisible: boolean, Group: any,
+    SortOrder: number, RouteTraceFilename: string, RouteTraceHash64: any,
+    IsHeadway: boolean, IncludeInGoogle: boolean, GoogleDescription: string,
+    Stops: Stop[], RouteStops: any[], Directions: any[], Vehicles: Vehicle[],
+    Messages: Alert[]) {
+    this.RouteId = RouteId;
+    this.RouteRecordId = RouteRecordId;
+    this.ShortName = ShortName;
+    this.LongName = LongName;
+    this.RouteAbbreviation = RouteAbbreviation;
+    this.IvrDescription = IvrDescription;
+    this.Color = Color;
+    this.TextColor = TextColor;
+    this.IsVisible = IsVisible;
+    this.Group = Group;
+    this.SortOrder = SortOrder;
+    this.RouteTraceFilename = RouteTraceFilename;
+    this.RouteTraceHash64 = RouteTraceHash64;
+    this.IsHeadway = IsHeadway;
+    this.IncludeInGoogle = IncludeInGoogle;
+    this.GoogleDescription = GoogleDescription;
+    this.Stops = Stops;
+    this.RouteStops = RouteStops;
+    this.Directions = Directions;
+    this.Vehicles = Vehicles;
+    this.Messages = Messages;
   }
 }
--- src/models/route.ts
+++ src/models/route.ts
@@ -1,12 +1,54 @@
 class Route {
-  shortName: string;
-  longName: string;
-  color: number;
-  routeId: number;
-  constructor(shortName: string, longName: string, color: number, routeId: number){
-    this.shortName = shortName;
-    this.longName = longName;
-    this.color = color;
-    this.routeId = routeId;
+  RouteId: number;
+  RouteRecordId: number;
+  ShortName: string;
+  LongName: string;
+  RouteAbbreviation: string;
+  IvrDescription: string;
+  Color: string;
+  TextColor: string;
+  IsVisible: boolean;
+  Group: any;
+  SortOrder: number;
+  RouteTraceFilename: string;
+  RouteTraceHash64: any;
+  IsHeadway: boolean;
+  IncludeInGoogle: boolean;
+  GoogleDescription: string;
+  Stops: any;
+  RouteStops: any;
+  Directions: any;
+  Vehicles: Vehicle[];
+  Messages: Alert[];
+
+  constructor(RouteId: number, RouteRecordId: number, ShortName: string,
+    LongName: string, RouteAbbreviation: string, IvrDescription: string,
+    Color: string, TextColor: string, IsVisible: boolean, Group: any,
+    SortOrder: number, RouteTraceFilename: string, RouteTraceHash64: any,
+    IsHeadway: boolean, IncludeInGoogle: boolean, GoogleDescription: string,
+    Stops: any, RouteStops: any, Directions: any, Vehicles: Vehicle[],
+    Messages: Alert[]){
+      this.RouteId = RouteId;
+      this.RouteRecordId = RouteRecordId;
+      this.ShortName = ShortName;
+      this.LongName = LongName;
+      this.RouteAbbreviation = RouteAbbreviation;
+      this.IvrDescription = IvrDescription;
+      this.Color = Color;
+      this.TextColor = TextColor;
+      this.IsVisible = IsVisible;
+      this.Group = Group;
+      this.SortOrder = SortOrder;
+      this.RouteTraceFilename = RouteTraceFilename;
+      this.RouteTraceHash64 = RouteTraceHash64;
+      this.IsHeadway = IsHeadway;
+      this.IncludeInGoogle = IncludeInGoogle;
+      this.GoogleDescription = GoogleDescription;
+      this.Stops = Stops;
+      this.RouteStops = RouteStops;
+      this.Directions = Directions;
+      this.Vehicles = Vehicles;
+      this.Messages = Messages;
+
   }
 }
