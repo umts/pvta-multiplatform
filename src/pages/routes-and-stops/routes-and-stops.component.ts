dc3cddad951a6298bfff670df4fc8af873f5e99f
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-12-18 22:34:20 -0500

    Working on r&s search

--- src/pages/routes-and-stops/routes-and-stops.component.ts
+++ src/pages/routes-and-stops/routes-and-stops.component.ts
@@ -19,8 +19,14 @@ export class RoutesAndStops implements OnInit {
   routes: Route[];
   stops: Stop[];
   currentDisplay: string = '0';
+  searchQuery: string = '';
   constructor(public navCtrl: NavController,
     private routeService: RouteService, private stopService: StopService) { }
+
+  onSearchQueryChanged(event, query): void {
+      console.log(JSON.stringify(event));
+      console.log(JSON.stringify(query));
+  }
 
   ngOnInit(): void {
     console.log('kjdflkdsjf');
