d71647ccd25577f2459d6ebd8be3df0e11abb57b
Matt Moretti <werebus@gmail.com>
2016-12-23 15:55:24 -0500

    Add task to link the stage api keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,12 +16,6 @@ set :rsync_opts, '--recursive --links --times --perms --chmod=g+w'
 
 set :log_level, :info
 
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
 namespace :scm do
   task :check do
     invoke 'local_copy:check'
@@ -30,6 +24,7 @@ namespace :scm do
   task :create_release do
     invoke 'local_copy:create_release'
     invoke 'local_copy:upload_release'
+    invoke 'api_keys:link'
   end
 
   task :set_current_revision do
