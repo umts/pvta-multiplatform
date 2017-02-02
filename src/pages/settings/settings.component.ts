5f1c268bdc2f33dd95d84c8ed14015b2e9abdbfd
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-02-01 21:37:12 -0500

    Convert most of the settings UI

--- src/pages/settings/settings.component.ts
+++ src/pages/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'page-settings',
@@ -10,6 +11,10 @@ export class SettingsComponent {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToPg() {
+    this.navCtrl.push(AboutComponent)
   }
 
 }
