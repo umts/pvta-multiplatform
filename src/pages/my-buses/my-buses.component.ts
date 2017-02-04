faec6f6b48fc924eee52c2df3973479bd7926ad9
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-02-04 16:09:22 -0500

    Begin building out mybuses UI

--- src/pages/my-buses/my-buses.component.ts
+++ src/pages/my-buses/my-buses.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { StopComponent } from '../stop/stop.component';
+import { RouteComponent } from '../route/route.component';
 
 @Component({
   selector: 'page-my-buses',
@@ -12,4 +14,66 @@ export class MyBusesComponent {
 
   }
 
+  stops: any = [
+    {
+      Description: "The Boulders Apts",
+      StopId: 157
+    },
+    {
+      Description: "Arnold House",
+      StopId: 56
+    },
+    {
+      Description: "Fine Arts Center",
+      StopId: 71
+    }
+  ];
+
+  routes: any = [
+    {
+      Color: "C7A020",
+      GoogleDescription: "North Amherst / Old Belchertown Rd",
+      RouteAbbreviation: "30",
+      RouteId: 20030,
+      ShortName:"30"
+    },
+    {
+      Color: "C7A020",
+      GoogleDescription: "Sunderland / South Amherst",
+      RouteAbbreviation: "31",
+      RouteId: 20031,
+      ShortName:"31"
+    }
+  ];
+
+  messages: any = [
+      {
+        MessageId: 0,
+        Message: "The Route 30 is bypassing Colonial Village due to snow. Catch buses on Route 9.",
+        FromDate: "2017-02-04T20:24:42.916Z",
+        ToDate: "2017-02-04T20:24:42.916Z",
+        FromTime: "2017-02-04T20:24:42.916Z",
+        ToTime: "2017-02-04T20:24:42.916Z",
+        Priority: 0,
+        DaysOfWeek: 1,
+        Published: true,
+        PublicAccess: 0,
+        Routes: [
+          20030
+        ],
+        Signs: [],
+        MessageTranslations: [],
+        ChannelMessages: []
+      }
+    ];
+    goToStopPage(stopId: number): void {
+      this.navCtrl.push(StopComponent, {
+        stopId: stopId
+      });
+    }
+    goToRoutePage(routeId: number): void {
+      this.navCtrl.push(RouteComponent, {
+        routeId: routeId
+      });
+    }
 }
