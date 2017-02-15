580178a82e4d7a148faeaaad7faab8f9b3de78b4
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-02-14 21:28:00 -0500

    Search bar on R&S is functional

--- src/pages/routes-and-stops/routes-and-stops.component.ts
+++ src/pages/routes-and-stops/routes-and-stops.component.ts
@@ -10,31 +10,57 @@ import { Route } from '../../models/route.model';
 import { Stop } from '../../models/stop.model';
 import { RouteComponent } from '../route/route.component';
 import { StopComponent } from '../stop/stop.component'
+import * as _ from 'lodash';
 
-
+export enum SegmentDisplay {
+    Routes,
+    Stops,
+}
 
 @Component({
   selector: 'page-routes-and-stops',
   templateUrl: 'routes-and-stops.html'
 })
+
 export class RoutesAndStopsComponent {
   routes: Route[];
   stops: Stop[];
-  currentDisplay: string = '0';
+  // currentDisplay: string = '0';
+  segment = SegmentDisplay;
+  cDisplay: SegmentDisplay;
   searchQuery: string = '';
   stopsDisp: Stop[];
   routesDisp: Route[];
   constructor(public navCtrl: NavController,
-    private routeService: RouteService, private stopService: StopService) { }
+    private routeService: RouteService, private stopService: StopService) {
+      this.cDisplay = SegmentDisplay.Routes;
+    }
 
   onSearchQueryChanged(event: any): void {
-      console.log(JSON.stringify(event.target.value));
-      if (this.currentDisplay == '0') {
-
+    let query: string = event.target.value.toLowerCase().trim();
+    console.log(query);
+    if (query == '') {
+      this.routesDisp = this.routes;
+      this.stopsDisp = this.stops;
+    }
+    else {
+      if (this.cDisplay == SegmentDisplay.Routes) {
+        // this.routesDisp = this.routes;
+        this.routesDisp = _.filter(this.routes, route => {
+          // console.log(route.LongName, " ", route.RouteAbbreviation);
+          return (route.LongName.toLowerCase().includes(query) || route.RouteAbbreviation.toLowerCase().includes(query));
+        });
+        // console.log(this.routesDisp);
       }
-      else {
-
+      else if (this.cDisplay = SegmentDisplay.Stops){
+        // console.log(query, "qeen");
+        // this.stopsDisp = this.stops;
+        this.stopsDisp = _.filter(this.stops, stop => {
+          // console.log(stop.Description, query, stop.Description.toLowerCase().includes(query));
+          return stop.Description.toLowerCase().includes(query);
+        });
       }
+    }
   }
 
   redirectRoute(routeId: number): void {
