eef3c6ab4c6305419a946b25891b9c4f3926e900
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-03-09 23:11:16 -0500

    Some deletions

--- src/pages/plan-trip/plan-trip.component.ts
+++ src/pages/plan-trip/plan-trip.component.ts
@@ -13,25 +13,23 @@ declare var google;
 })
 export class PlanTripComponent {
   @ViewChild('directionsMap') mapElement: ElementRef;
-  defaultMapCenter = new google.maps.LatLng(42.3918143, -72.5291417);//Coords for UMass Campus Center
+  // defaultMapCenter = new google.maps.LatLng(42.3918143, -72.5291417);//Coords for UMass Campus Center
   // These coordinates draw a rectangle around all PVTA-serviced area. Used to restrict requested locations to only PVTALand
    swBound = new google.maps.LatLng(41.93335, -72.85809);
    neBound = new google.maps.LatLng(42.51138, -72.20302);
    bounds = new google.maps.LatLngBounds(this.swBound, this.neBound);
    request;
-   originForm: string = '';
-   destionationForm: string = '';
+
    originPlace;
    originInput: string = '';
    destinationPlace;
    noLocation: boolean;
-   loadedTrip;
+  //  loadedTrip;
    destinationInput;
    directionsDisplay;
    map;
    route;
    loader;
-   placeService;
    timeOptions = []
 
   constructor(public navCtrl: NavController, private stopService: StopService,
@@ -43,7 +41,6 @@ export class PlanTripComponent {
      * isASAP: whether we should ignore all other given times and
                request a trip leaving NOW
       */
-    this.mapbounds = new google.maps.LatLngBounds();
     this.timeOptions = [
       { title: 'Leaving Now', type: 'departure', isASAP: true, id: 0 },
       { title: 'Departing At', type: 'departure', isASAP: false,id: 1 },
@@ -75,12 +72,14 @@ export class PlanTripComponent {
         },
         (results, status) => {
           if (status === google.maps.GeocoderStatus.OK) {
-            if (results[1]) {
-              this.originPlace = results[1];
-              this.originInput = results[1].formatted_address;
+            console.log(results);
+            if (results[0]) {
+              let closestGeolocationAddress = results[0];
+              this.originPlace = closestGeolocationAddress;
+              this.originInput = closestGeolocationAddress.formatted_address;
               this.request.origin = {
-                name: results[1].formatted_address,
-                id: results[1].place_id
+                name: closestGeolocationAddress.formatted_address,
+                id: closestGeolocationAddress.place_id
               };
               if (this.request.destination.name) {
                 this.request.name = this.request.destination.name;
@@ -120,19 +119,19 @@ export class PlanTripComponent {
 
   /* Called when this page is opened, and either a loaded trip has been queued
    * or there are no current existing parameters. Also called as a result of the
-   * newTrip method. Constructs the map, and then sets $scope.request as either default
+   * newTrip method. Constructs the map, and then sets this.request as either default
    * or loaded parameters */
-  reload(): void {
+  reload(loadedTrip): void {
     // @TODO Scroll to input area first
-    this.constructMap(this.defaultMapCenter);
+    this.constructMap();
     this.noLocation = false;
     // If we loaded a trip (user came via
     // saved trip on My Buses), pull out
     // its details and display them.
-    if (this.loadedTrip) {
-      this.request = this.loadedTrip;
+    if (loadedTrip) {
+      this.request = loadedTrip;
       this.request.time.option = this.timeOptions[this.request.time.option];
-      this.loadedTrip = null;
+      loadedTrip = null;
       // If the datetime of loeaded trip is in the past,
       // keep the time and update the date to today. Else do nothing.
       if (this.request.time.datetime < Date.now()) {
@@ -173,15 +172,13 @@ export class PlanTripComponent {
 
   ionViewWillEnter() {
     // @TODO Load saved trips
+    let loadedTrip = null;
     // this.loadedTrip = Trips.pop();
-    //@TODO reload if either a trip is being loaded or if this page has not yet been loaded
-    // if (loadedTrip || !$scope.request)
-    this.reload();
+    this.reload(loadedTrip);
   }
 
-  constructMap(latLng: any): void {
+  constructMap(): void {
     var mapOptions = {
-      center: latLng,
       zoom: 15,
       mapTypeControl: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -190,20 +187,20 @@ export class PlanTripComponent {
     this.directionsDisplay = new google.maps.DirectionsRenderer;
     this.directionsDisplay.setMap(this.map);
 
-    var originInput = <HTMLInputElement>document.getElementById('origin-input');
-    var destinationInput = <HTMLInputElement>document.getElementById('destination-input');
+    let originInput = <HTMLInputElement>document.getElementById('origin-input');
+    let destinationInput = <HTMLInputElement>document.getElementById('destination-input');
 
-    var originAutocomplete = new google.maps.places.Autocomplete(originInput);
-    var destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
-    this.placeService = new google.maps.places.PlacesService(this.map);
+    let originAutocomplete = new google.maps.places.Autocomplete(originInput);
+    let destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
+
     originAutocomplete.setBounds(this.bounds);
     destinationAutocomplete.setBounds(this.bounds);
     // When the user has selected a valid Place from the dropdown
     originAutocomplete.addListener('place_changed', () => {
       let place = originAutocomplete.getPlace();
-      // debugger;
       if (!place || !place.geometry) {
-        this.presentAlert('Invalid Origin', 'Choose a location from the list of suggestions.');
+        this.presentAlert('Invalid Origin',
+        'Choose a location from the list of suggestions.');
         // ga('send', 'event', 'AutocompleteFailure', 'originAutocomplete.place_changed', 'autocomplete failure in plan trip: ' + err);
         // If the location chosen is not valid, an error is thrown.
         // request.origin.name still holds the text that the user
@@ -213,8 +210,9 @@ export class PlanTripComponent {
         originInput.value = this.request.origin.name;
         console.error('No geometry, invalid input.');
       } else if (!this.bounds.contains(place.geometry.location)) {
-        this.presentAlert('Invalid Destination', 'The PVTA does not service this location.');
-        console.error('Location ' + place.name + ' is out of bounds. ID: ' + place.id);
+        this.presentAlert('Invalid Origin',
+        'The PVTA does not service this location.');
+        console.error(`Location ${place.name} is out of bounds.`);
       } else {
         this.originPlace = place;
         this.request.origin = {
@@ -222,51 +220,33 @@ export class PlanTripComponent {
           id: place.place_id
         };
         this.request.destinationOnly = false;
-        // Give the trip a default name if it has an origin AND destination.
-        if (this.request.destination.name) {
-          this.request.name = place.name + ' to ' + this.request.destination.name;
-        }
       }
     });
     // When a valid destination is chosen:
     destinationAutocomplete.addListener('place_changed', () => {
       let place =  destinationAutocomplete.getPlace();
       if (!place || !place.geometry) {
-        // @TODO Add invalidLocationPopup
-        this.presentAlert('Invalid Destination', 'Choose a location from the list of suggestions.');
-        // invalidLocationPopup('Choose a location from the list of suggestions.');
+        this.presentAlert('Invalid Destination',
+        'Choose a location from the list of suggestions.');
         console.error('No geometry, invalid input.');
       }
       else if (!this.bounds.contains(place.geometry.location)) {
-        this.presentAlert('Invalid Destination', 'The PVTA does not service this location.');
-        console.error('Location ' + place.name + ' is out of bounds. ID: ' + place.id);
+        this.presentAlert('Invalid Destination',
+        'The PVTA does not service this location.');
+        console.error(`Location ${place.name} is out of bounds.`);
       } else {
         this.destinationPlace = place;
         this.request.destination = {
           name: place.name,
           id: place.place_id
         };
-        // Name the trip
-        if (this.request.destinationOnly || !this.request.origin.name) {
-          this.request.name = place.name;
-        } else {
-          // ORIGIN to DESTINATION if not destinationOnly
-          this.request.name = this.request.origin.name + ' to ' + place.name;
-        }
       }
     }, (err) => {
         // ga('send', 'event', 'AutocompleteFailure', 'destinationAutocomplete.place_changed', 'autocomplete failure in plan trip: ' + err);
-        //See comments for originAutocompleteListener method
+        // See comments for originAutocompleteListener method
         this.request.destination.id = null;
         destinationInput.value = this.request.destination.name;
     });
-  }
-  mapbounds;
-  mapLocation(place): void {
-      //Fit the location on the map
-      // debugger;
-      console.log('totoitoto')
-      this.mapbounds.extend(place.geometry.location);
   }
 
   presentAlert(title: string, body: string): void {
@@ -276,29 +256,6 @@ export class PlanTripComponent {
       buttons: ['Dismiss']
     });
     alert.present();
-  }
-
-  checkForValidPlace(type: string, status) {
-    if (status != google.maps.places.PlacesServiceStatus.OK) {
-      this.presentAlert(`Invalid ${type}`, `Unable to find a route. Error code: ${status}`)
-    }
-  }
-
-  setRequestOrigin(place): void {
-    console.log('origin place', place);
-    this.originInput = place.formatted_address;
-    this.request.origin = {
-      name: place.formatted_address,
-      id: place.place_id
-    };
-  }
-  setRequestDestination(place): void {
-    console.log('destination place', place);
-     this.destinationInput = place.formatted_address;
-    this.request.destination = {
-      name: place.formatted_address,
-      id: place.place_id
-    };
   }
 
   /* Requests a trip from Google using the trip params.
@@ -333,7 +290,7 @@ export class PlanTripComponent {
         // User wants arrival in the future? Pass the time.
         transitOptions['arrivalTime'] = new Date(this.request.time.datetime);
       } else {
-        console.error('Determining route for Plan Trip failed due to unexpected input. Expected \'arrival\' or \'departure\', received');
+        this.presentAlert('Error', 'Received invalid time');
         // ga('send', 'event', 'RoutingParamsInvalid', 'PlanTripController.getRoute()', 'Received invalid time params for planning a route');
         this.loader.dismiss();
         return;
@@ -343,74 +300,43 @@ export class PlanTripComponent {
     /*
      * Send the official request to Google!
      */
-    //  this.route = []
     directionsService.route(
       {
         origin: {'placeId': this.request.origin.id},
         destination: {'placeId': this.request.destination.id},
         travelMode: google.maps.TravelMode.TRANSIT,
         transitOptions: transitOptions
-      },
-      (response, status) => {
-      console.log('directionsresponse', response, 'directionstatus', status);
-      // @TODO figure out whether to add this back in
-      // && this.confirmValidRoute(response.routes[0])
+      }, (response, status) => {
       if (status === google.maps.DirectionsStatus.OK ) {
+        // Force a map redraw because it was hidden before.
+        // There's an angular bug (with [hidden]) that will cause
+        // the map to draw only grey after being hidden
+        // unless we force a redraw TWICE after delays -_-
         setTimeout(() => {
           this.loader.dismiss();
           google.maps.event.trigger(this.map, 'resize');
-          // this.map.setZoom(10);
           this.directionsDisplay.setDirections(response);
           this.route = response.routes[0].legs[0];
-          console.log('map idle')
-          console.log(this.originPlace);
-          console.log(this.destinationPlace);
-          // this.mapLocation(this.originPlace);
-          // this.mapLocation(this.destinationPlace);
-          // this.map.fitBounds(this.mapbounds);
         }, 1000);
         setTimeout(() => {
           google.maps.event.trigger(this.map, 'resize');
-          console.log('tits');
           this.directionsDisplay.setDirections(response);
         }, 2000)
-        // @TODO scroll to route being displayed in UI
-        // $scope.scrollTo('route');
 
-        // Force a map redraw because it was hidden before.
-        // There's STILL an angular bug (with [hidden]) that will cause
-        // the map to draw only grey after being hidden
-        // unless we force a redraw after a brief delay.
         // ga('send', 'event', 'TripStepsRetrieved', ser 'PlanTripController.getRoute()', 'Received steps for a planned trip!');
       } else  {
         console.log(status);
-        // @TODO Add the popup
-        // $ionicPopup.alert(
-        //   {
-        //     title: 'Unable to Find Trip',
-        //     template: 'There are no scheduled buses for your trip.<br>Status Code: ' + status
-        //   }
-        // );
+        this.presentAlert('Unable to Find Trip', `There are no scheduled buses for your trip. Error: ${status}`);
         // ga('send', 'event', 'TripStepsRetrievalFailure', 'PlanTripController.$scope.getRoute()', 'Unable to get a route; error: ' + status);
 
         // In cases of error, we set the route object that
         // otherwise contained all our data to undefined, because, well,
         // the data was bad.
-        this.route = undefined;
+        this.route = null;
       }
     });
-    // this.loader.dismiss();
-    // @TODO figure out of this should be a thing?
-    // , function (err) {
-    //   this.route = undefined;
-    //   // $ionicLoading.hide();
-    //   console.log('Error routing: ' + err);
-    //   // ga('send', 'event', 'TripStepsRoutingFailure', 'PlanTripController.$scope.getRoute()', 'Trip Factory unable to get a route due to some error: ' + err);
-    // });
   }
-  confirmValidRoute(route): boolean {
-    return !(route.legs[0].steps.length === 1 && route.legs[0].steps[0]['travel_mode'] === 'WALKING');
-  }
+
   /*
    * Saves the current trip parameters to the db
    * for display on My Buses
