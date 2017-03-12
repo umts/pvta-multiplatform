d5175f2edcb0a45eab7007e91f358c8c49d595c7
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-03-11 19:27:37 -0500

    figured out date pipe polyfill - import node module in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { MyApp } from './app.component';
+import 'intl';
+import 'intl/locale-data/jsonp/en';
 
 // Pages
 import { AboutComponent } from '../pages/about/about.component';
