765166e09fb84baa5f32629d9bfc4ff316be7be5
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-03-17 14:04:38 -0400

    Move folders to proper locations

--- src/config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-remote_user = Net::SSH::Config.for('af-transit-app3.admin.umass.edu')[:user] || ENV['USER']
-server 'af-transit-app3.admin.umass.edu',
-       roles: %w(web),
-       user: remote_user
-set :tmp_dir, "/tmp/#{remote_user}"
