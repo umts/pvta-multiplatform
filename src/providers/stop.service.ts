b2239c542d3cd2ab6364535acad88faad62220f1
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-03-31 20:56:59 -0400

    Add pagination to the stop modal. No more 'too many stops to show,' required changing filter function too

--- src/providers/stop.service.ts
+++ src/providers/stop.service.ts
@@ -72,7 +72,7 @@ export class StopService {
   }
   filterStopsByQuery(stops: Stop[], query: string): Stop[] {
     if (!query || query === '') {
-      return [];
+      return stops;
     }
     query = query.toLowerCase().trim();
     return _.filter(stops, stop => {
