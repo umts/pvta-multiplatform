016d81ae91aaf07f3f8141d8c53807fafd083435
Matt Moretti <werebus@gmail.com>
2017-04-07 12:49:04 -0400

    Added webpack plugin
    This populates a special "define" called `ENV` which you can use in your
    code. It gets replaced at compile-time with the values from the relevant json
    file in the `env/` directory.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var projectRootDir = process.env.IONIC_ROOT_DIR;
+var appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR;
+
+var config = require(path.join(appScriptsDir, 'config', 'webpack.config.js'));
+
+var env = process.env.IONIC_ENV || 'dev';
+var envVars;
+try {
+  envVars = require(path.join(projectRootDir, 'env', env + '.json'));
+} catch(e) {
+  envVars = {};
+}
+
+config.plugins = config.plugins || []
+config.plugins.push(
+  new webpack.DefinePlugin({
+    ENV: Object.assign(envVars, {
+      environment: JSON.stringify(env)
+    })
+  })
+);
+
+if (env === 'prod') {
+  config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+}
+
+module.exports = config;
