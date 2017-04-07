116991ff9b63fa67650cd8afe82ec480f8dcd542
Matt Moretti <werebus@gmail.com>
2017-04-07 12:53:26 -0400

    Use ENV for API keys

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+declare const ENV;
+declare var ga;
+
+var head = document.getElementsByTagName('head')[0];
+var mapsApi = document.createElement('script');
+mapsApi.src = 'https://maps.googleapis.com/maps/api/js?libraries=places,geometry&key=' + ENV.gmaps_key;
+head.appendChild(mapsApi);
+
+if (ENV.ga) {
+  ga('create', ENV.ga_id, 'auto');
+  var analyticsMeta = document.createElement('meta');
+  analyticsMeta.name = 'google-site-verification';
+  analyticsMeta.content = ENV.ga_meta;
+  head.appendChild(analyticsMeta);
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule);
