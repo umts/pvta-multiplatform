e4492312c61063888e18f2b651204d84a68edf41
Matt Moretti <werebus@gmail.com>
2017-04-07 13:44:47 -0400

    Moved `ga(...)` function to its own file

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,14 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
 
-declare const ENV;
-declare var ga;
+import { gaInit } from './ga.ts';
+gaInit();
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+declare const ENV;
 
 var head = document.getElementsByTagName('head')[0];
 var mapsApi = document.createElement('script');
@@ -17,6 +13,7 @@ head.appendChild(mapsApi);
 
 if (ENV.ga) {
   ga('create', ENV.ga_id, 'auto');
+
   var analyticsMeta = document.createElement('meta');
   analyticsMeta.name = 'google-site-verification';
   analyticsMeta.content = ENV.ga_meta;
