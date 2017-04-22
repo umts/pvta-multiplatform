45391a3b459d9d4ef4b6e4397f87fa4eb92d1051
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-04-22 19:31:29 -0400

    FIXED MAPS API LOADING IN TIME ISSUE YAY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ export class MyApp {
 
   constructor(public platform: Platform, private infoSvc: InfoService,
   private connectivityService: ConnectivityService) {
+
     this.initializeApp();
 
     // used for an example of ngFor and navigation
