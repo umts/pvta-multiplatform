fa31e8909c76636c2465ea73f9f1d79c0d822e51
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-05-04 18:31:34 -0400

    add stop map spec

---
+++ src/pages/stop-map/stop-map.component.spec.ts
@@ -0,0 +1,58 @@
+import { async, TestBed } from '@angular/core/testing';
+import { MyApp } from '../../app/app.component';
+import {} from 'jasmine';
+import { IonicStorageModule } from '@ionic/storage';
+import { IonicModule, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+import { StopService } from '../../providers/stop.service';
+import { VehicleService } from '../../providers/vehicle.service';
+import { MapService } from '../../providers/map.service';
+import { AutoRefreshService } from '../../providers/auto-refresh.service';
+import { ConnectivityService } from '../../providers/connectivity.service';
+import { StopMapComponent } from './stop-map.component';
+import { Geolocation } from '@ionic-native/geolocation';
+import { NavParamsMock } from '../../../test-config/mocks-ionic';
+
+describe('StopMap Component', () => {
+  let fixture;
+  let component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MyApp, StopMapComponent],
+      imports: [
+        IonicModule.forRoot(MyApp),
+        IonicStorageModule.forRoot({name: 'test', storeName: 'test'})
+      ],
+      providers: [
+        { provide: NavParams, useClass: NavParamsMock },
+        StopService,
+        MapService,
+        ToastController,
+        ConnectivityService,
+        LoadingController,
+        Geolocation
+      ]
+    })
+  }));
+
+  beforeEach(() => {
+    (<any> window).ga = jasmine.createSpy('ga');
+    fixture = TestBed.createComponent(StopMapComponent);
+    component = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
+    (<any> window).ga = undefined;
+
+  });
+
+  it ('should be created', () => {
+    expect(component instanceof StopMapComponent).toBe(true);
+  });
+  it('sends a pageview to Google Analytics', () => {
+    expect((<any>window).ga.calls.allArgs()).toContain(
+    ['set', 'page', '/stop/stop-map.html'])
+  });
+});
