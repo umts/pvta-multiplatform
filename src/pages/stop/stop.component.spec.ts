d7dcdd48a5bdb1322720285fdfd7b55ae0c846c3
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-05-04 22:04:39 -0400

    Add stop component spec

---
+++ src/pages/stop/stop.component.spec.ts
@@ -0,0 +1,63 @@
+import { async, TestBed } from '@angular/core/testing';
+import { MyApp } from '../../app/app.component';
+import {} from 'jasmine';
+import { IonicStorageModule } from '@ionic/storage';
+import { IonicModule, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+import { RouteService } from '../../providers/route.service';
+import { StopService } from '../../providers/stop.service';
+import { StopDepartureService } from '../../providers/stop-departure.service';
+import { InfoService } from '../../providers/info.service';
+import { AutoRefreshService } from '../../providers/auto-refresh.service';
+import { FavoriteStopService } from '../../providers/favorite-stop.service';
+import { ConnectivityService } from '../../providers/connectivity.service';
+import { StopComponent } from './stop.component';
+import { NavParamsMock } from '../../../test-config/mocks-ionic';
+
+describe('Stop Component', () => {
+  let fixture;
+  let component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MyApp, StopComponent],
+      imports: [
+        IonicModule.forRoot(MyApp),
+        IonicStorageModule.forRoot({name: 'test', storeName: 'test'})
+      ],
+      providers: [
+        NavController,
+        { provide: NavParams, useClass: NavParamsMock },
+        StopDepartureService,
+        InfoService,
+        RouteService,
+        LoadingController,
+        FavoriteStopService,
+        StopService,
+        ConnectivityService,
+        AutoRefreshService,
+        AlertController
+      ]
+    })
+  }));
+
+  beforeEach(() => {
+    (<any> window).ga = jasmine.createSpy('ga');
+    fixture = TestBed.createComponent(StopComponent);
+    component = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
+    (<any> window).ga = undefined;
+
+  });
+
+  it ('should be created', () => {
+    expect(component instanceof StopComponent).toBe(true);
+  });
+  it('sends a pageview to Google Analytics', () => {
+    expect((<any>window).ga.calls.allArgs()).toContain(
+    ['set', 'page', '/stop.html'])
+  });
+});
