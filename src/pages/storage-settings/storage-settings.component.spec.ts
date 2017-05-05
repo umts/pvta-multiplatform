b8a9e9bcb51d30133a02c7389ff6ffd59a78ca89
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-05-04 22:12:13 -0400

    Add storage settings spec

---
+++ src/pages/storage-settings/storage-settings.component.spec.ts
@@ -0,0 +1,47 @@
+import { async, TestBed } from '@angular/core/testing';
+import { IonicModule, NavController, Platform, ToastController, AlertController } from 'ionic-angular';
+import { StorageSettingsComponent } from './storage-settings.component';
+import { MyApp } from '../../app/app.component';
+import {} from 'jasmine';
+import { IonicStorageModule } from '@ionic/storage';
+
+describe('StorageSettings Component', () => {
+  let fixture;
+  let component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MyApp, StorageSettingsComponent],
+      imports: [
+        IonicModule.forRoot(MyApp),
+        IonicStorageModule.forRoot({name: 'test', storeName: 'test'})
+      ],
+      providers: [
+        NavController,
+        ToastController,
+        AlertController
+      ]
+    })
+  }));
+
+  beforeEach(() => {
+    (<any> window).ga = jasmine.createSpy('ga');
+    fixture = TestBed.createComponent(StorageSettingsComponent);
+    component = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
+    (<any> window).ga = undefined;
+
+  });
+
+  it ('should be created', () => {
+    expect(component instanceof StorageSettingsComponent).toBe(true);
+  });
+  it('sends a pageview to Google Analytics', () => {
+    expect((<any>window).ga.calls.allArgs()).toContain(
+    ['set', 'page', '/settings/storage-settings.html'])
+  });
+});
