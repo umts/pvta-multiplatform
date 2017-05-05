c4febe4811468cff792b929cedf26718ae447098
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-05-05 18:11:20 -0400

    Karma - generate lcov reports. Travis - send them to codeclimate

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
       terminal: false
     },
     coverageIstanbulReporter: {
-      reports: [ 'json-summary' ],
+      reports: [ 'lcov' ],
       fixWebpackSourcePaths: true
     },
     reporters: ['spec', 'coverage-istanbul'],
