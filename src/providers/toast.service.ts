cf9958895c29d3110422614aacdebee611b28199
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-06-27 17:26:37 -0400

    Eliminate lengthy code in ToastService

--- src/providers/toast.service.ts
+++ src/providers/toast.service.ts
@@ -5,12 +5,8 @@ import { ToastController } from 'ionic-angular';
 export class ToastService {
     private faveToast; 
     constructor(private toast: ToastController) { } 
-    
-    favoriteToast(routeOrStop: string, isFave: boolean): void {
-        if (isFave){ 
-          this.faveToast = this.toast.create({message: routeOrStop + ' added to Favorites', position: 'bottom', showCloseButton: true});
-        }
-        else this.faveToast = this.toast.create({message: routeOrStop + ' removed from Favorites', position: 'bottom', showCloseButton: true});
-    this.faveToast.present();
-    }
+
+  let txt = `${routeOrStop} ${isFave ? 'added' : 'removed'} from favorites`;
+  this.toast.create({message: txt, position: 'bottom', showCloseButton: true});
+  this.toast.present();
 }
