967ea6824000f107129aa7ec2fdece1fe58c0146
Molly O'Neil <mboneil@umass.edu>
2017-06-28 10:36:14 -0400

    Change 'favorites' to 'Favorites' in toast message

--- src/providers/toast.service.ts
+++ src/providers/toast.service.ts
@@ -7,7 +7,7 @@ export class ToastService {
     constructor(private toast: ToastController) { } 
   
   favoriteToast(routeOrStop: string, isFave: boolean): void {
-    let txt = `${routeOrStop} ${isFave ? 'added' : 'removed'} from favorites`;
+    let txt = `${routeOrStop} ${isFave ? 'added' : 'removed'} from Favorites`;
     this.faveToast = this.toast.create({message: txt, position: 'bottom', showCloseButton: true});
     this.faveToast.present();
   }
