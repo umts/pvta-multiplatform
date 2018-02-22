088fe01525b211cb8f2346e763e97e8c56290da0
Arta <artarazavi@gmail.com>
2018-02-22 15:12:25 -0500

    fixed class wide declaration

--- src/providers/toast.service.ts
+++ src/providers/toast.service.ts
@@ -4,16 +4,12 @@ import { ToastController } from 'ionic-angular';
 @Injectable()
 export class ToastService {
     private faveToast;
-    private noLocationToast;
-    private directToast;
-    private directionStatus;
-    private originDestination;
     constructor(private toast: ToastController) { }
 
   toastHandler(text: string){
 		let txt = text;
-		this.toastHandle = this.toast.create({message: txt, position: 'bottom', showCloseButton: true});
-		this.toastHandle.present();
+		private toastHandle = this.toast.create({message: txt, position: 'bottom', showCloseButton: true});
+		toastHandle.present();
   }
   favoriteToast(routeOrStop: string, isFave: boolean): void {
     if (this.faveToast) {
@@ -25,7 +21,7 @@ export class ToastService {
   }
   noOriginOrDestinationToast(): void{
     let txt = 'You must select an origin and destination from the autocomplete dropdowns above in order to search the schedule';
-    this.originDestination = this.toast.create({message: txt, position: 'bottom', showCloseButton: true});
-    this.originDestination.present();
+    private originDestination = this.toast.create({message: txt, position: 'bottom', showCloseButton: true});
+    originDestination.present();
   }
 }
