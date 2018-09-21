ffec7e20594348f1606d57f4731f4341d7184d2a
Molly O'Neil <mboneil@umass.edu>
2018-09-21 16:02:24 -0400

    Update provider to account for all the API keys

--- src/providers/map.service.ts
+++ src/providers/map.service.ts
@@ -9,13 +9,25 @@ export class MapService {
   options = { timeout: 5000, enableHighAccuracy: true };
   markers = [];
   windows = [];
+  gmaps_key;
   constructor() {}
 
-  downloadGoogleMaps(cb: Function) {
+  downloadGoogleMaps(cb: Function, public platform: Platform) {
     window['mapsCb'] = cb;
     let head = document.getElementsByTagName('head')[0];
     let mapsApi = document.createElement('script');
-    mapsApi.src = `https://maps.googleapis.com/maps/api/js?libraries=places,geometry&key=${ENV.gmaps_key}&callback=mapsCb`;
+    if(this.platform.is('cordova') && (this.platform.is('core')){
+      if(ENV === 'prod'){
+        gmaps_key = ENV.gmaps_core;
+      } else {
+        gmaps_key = ENV.gmaps_dev;
+      }
+    } else {
+      // will need separate gmaps vars for android and ios
+      gmaps_key = ENV.gmaps_mobile;
+      }
+    }
+    mapsApi.src = `https://maps.googleapis.com/maps/api/js?libraries=places,geometry&key=${gmaps_key}&callback=mapsCb`;
     head.appendChild(mapsApi);
   }
 
