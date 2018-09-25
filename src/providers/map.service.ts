a198b1b01c428450e34ba9b5e3eafe7fcc6a8d00
Molly O'Neil <mboneil@umass.edu>
2018-09-25 14:30:16 -0400

    Oh yeah that's why I declared that arg

--- src/providers/map.service.ts
+++ src/providers/map.service.ts
@@ -12,7 +12,7 @@ export class MapService {
   gmaps_key;
   constructor() {}
 
-  downloadGoogleMaps(cb: Function) {
+  downloadGoogleMaps(cb: Function, public platform: Platform) {
     window['mapsCb'] = cb;
     let head = document.getElementsByTagName('head')[0];
     let mapsApi = document.createElement('script');
