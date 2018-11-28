e61aa8c0140c35ce3d325cfde5fa3e079541fd1d
Molly O'Neil <mboneil@umass.edu>
2018-11-28 13:36:04 -0500

    Our code isn't ready for this... yet

--- src/pages/plan-trip/plan-trip.component.ts
+++ src/pages/plan-trip/plan-trip.component.ts
@@ -61,6 +61,46 @@ export class PlanTripComponent {
   * Otherwise, clear out the values
   * for origin so the user knows to type something.
  */
+/*
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+      'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+}
+*/
  updateOrigin(): void {
    if (this.request.destinationOnly) {
      this.loadLocation();
